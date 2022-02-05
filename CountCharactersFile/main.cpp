8b77256882d8ddc7bf296c3d2ca879e2faf447c8
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2022-02-04 17:44:14 -0800

    Commit1

--- CountCharactersFile/main.cpp
+++ CountCharactersFile/main.cpp
@@ -1,14 +1,35 @@
-//
-//  main.cpp
-//  CountCharactersFile
-//
-//  Created by Abel Christy Anand on 2/4/22.
-//
-
+//Abel Anand
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+        string txt;
+        ifstream in;
+    
+        int spaces = 0;
+        int enter = 0;
+        int tab = 0;
+        int nums = 0;
+        int total = 0;
+        in.open("/Users/Abel/Desktop/File.txt");
+    
+    while (getline(in, txt)){
+        for (int i = 0; i < txt.length(); i++) {
+            total++;
+            if (txt[i] == ' ') {
+                spaces++;
+            }
+            else if(txt[i] == '\n'){
+                enter++;
+            }
+            else if(txt[i] == '\t'){
+                tab++;
+            }
+            else if(txt[i] >= 0 || txt[i] <= 9){
+                nums++;
+            }
+        }
+    }
+    cout << "Spaces: " << spaces << "\nNewlines: " << enter << "\nTabs: " << tab << "\nNumbers: " << nums << "\nTotal Characters: " << total << endl;
 }
