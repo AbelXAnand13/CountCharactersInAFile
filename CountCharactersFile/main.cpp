3a20646e0fad98cb555ef458a2e926ea7005142b
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2022-02-04 17:41:10 -0800

    Initial Commit

---
+++ CountCharactersFile/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CountCharactersFile
+//
+//  Created by Abel Christy Anand on 2/4/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
